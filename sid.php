b42ceba597a152a73a2fbe30f1cd7a306e9ee9d1
siddu1717 <94636697+siddu1717@users.noreply.github.com>
2022-02-11 09:46:52 +0530

    Create sid.php

---
+++ sid.php
@@ -0,0 +1,12 @@
+<body>
+<html>
+
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
+
+</body>
+</html>
